0e1baa7f18a9227fdef140b3eb622da1ef60094a
Allison <allisonkingwilliams@gmail.com>
2018-06-24 12:05:42 -0400

    changes to html and schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO cats (name) VALUES ('Squash');
+INSERT INTO cats (name) VALUES ('Burgers');
+INSERT INTO cats (name, sleepy) VALUES ('Corn on the cob', true);
+INSERT INTO cats (name, sleepy) VALUES ('Eggplant', true);
+INSERT INTO cats (name, sleepy) VALUES ('Chicken legs', true);
+INSERT INTO cats (name) VALUES ('Green Peppers');
